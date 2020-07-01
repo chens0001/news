1229a06b6eb478732dde9c35285160458ac79730
cs001 <727379405@qq.com>
2020-07-01 09:20:48 +0800

    123

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login">
+    login
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <div class="box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less'>
+.box{
+  width: 300px;
+  height: 300px;
+  background-color: pink;
+}
+
+</style>
